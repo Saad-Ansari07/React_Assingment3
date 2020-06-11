73487643e888113f9cabe65bb730aa948ca2d523
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-06-11 15:51:00 +0500

    Add files via upload

---
+++ src/room.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import  { useState } from 'react';
+
+function Room() {
+    const [islit, setlit] = useState(false);
+    let [age, setage] = useState(19);
+    function uplit() {
+        console.log('First button is clicked');
+        setlit(!islit);
+    }
+    function increaseAge() {
+        console.log('Second button is clicked');
+        setage(++age);
+    }
+return (
+<div>
+    This is the room where it happens and it's {islit? "lit": "dark"} and my age is {age}
+   <br/>
+    <button onClick={uplit}>Toggle Light</button>
+    <br/>
+    <button onClick={increaseAge}>Increase Age</button>
+
+</div>
+);
+}
+export default Room;
