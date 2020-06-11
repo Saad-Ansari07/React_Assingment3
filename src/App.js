fda8d1941b803f2ce87982d01045a3f07b4a49c6
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-06-11 15:50:19 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Room from './room';
+
+function App() {
+  return (
+    <div>
+      <Room></Room>
+    </div>
+  );
+}
+
+export default App;
